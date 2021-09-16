6cab4d93c8597f48a5973671954ebffff2cdcd06
Death768 <yiyoung.md@gmail.com>
2021-09-15 19:42:10 -0500

    string proccessing for spaces

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main() {
+void seperateBySpaces(string s, vector<string> &ret) {
+	int start = 0;
+	for(int i = 0; i < s.length(); i++) {
+		if(s[i] == ' ') {
+			ret.push_back(s.substr(start, i - start));
+			start = i + 1;
+		}
+	}
+	//check for edge cases
+	if(start == s.length()) return;
+	ret.push_back(s.substr(start, s.length()));
+}
 
+int main() {
+	while(true) {
+		string input;
+		getline(cin, input);
+
+		map<char, string> arguments;
+		
+		vector<string> spaceSeperated;
+		seperateBySpaces(input, spaceSeperated);
+
+		//check for options
+		//make sure options are valid
+	}
 	return 0;
 }
